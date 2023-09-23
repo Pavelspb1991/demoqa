5067902ce6349a0f111ef05b6a2dfc244713bb0f
Pavel <sn0oze@mail.ru>
2023-09-23 15:34:05 +0300

    123

--- tests/test_text_box.py
+++ tests/test_text_box.py
@@ -12,4 +12,6 @@ def test_text_box(browser):
     time.sleep(2)
     assert text_box.name_2.exist()
     assert text_box.current_address.exist()
-
+    # assert text_box.user_name.get_text() == text_box.name_2.get_text()
+    # assert text_box.current_address.get_text() == text_box.current_address.get_text()
+    # задачу с i* не понял как сравнивать ^ там с первом будет test,а внизу Name:test
